6084ecb767467d35e36d43f2f476f5ee796ee333
ilianachakarova <iliana1212@abv.bg>
2024-05-08 23:15:44 +0200

    first commit project setup

---
+++ src/main/java/com/example/ilianachakarovaemployees/IlianaChakarovaEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ilianachakarovaemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlianaChakarovaEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlianaChakarovaEmployeesApplication.class, args);
+    }
+
+}
